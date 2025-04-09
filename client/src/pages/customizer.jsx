3026f9052b913fbf72f1610147e389c45a975977
sftncnt <shayansiddique2005@gmail.com>
2025-04-09 09:58:07 +0400

    Built frontend

--- client/src/pages/customizer.jsx
+++ client/src/pages/customizer.jsx
@@ -58,15 +58,18 @@ const Customizer = () => {
     try {
       setGeneratingImage(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://threejs-ai-project-dhtd.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
